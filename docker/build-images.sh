c90e6043d23b956010645ba3e9dae458f3bce3a0
Thomas Pham <thomas.pham@ithings.ch>
2017-03-21 08:38:40 +0100

    groundwork for docker build and dev environment

---
+++ docker/build-images.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+(cd $DIR/.. && mvn clean install -Dmaven.test.skip=true)
+
+(cd $DIR/../api-gateway && mvn docker:build)
+(cd $DIR/../account-microservice && mvn docker:build)
+(cd $DIR/../cache-infrastructure && mvn docker:build)
+(cd $DIR/../monitor-dashboard && mvn docker:build)
